c117cb986bd59875e63da7b1cea71d45d0c0909f
David Hale and Riki Montgomery <riki@>
2017-08-28 10:20:06 -0700

    add form

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,1 @@
-$(document).ready(function() {
-  $("#lion").click(function() {
-    $("#lionInfo").show();
-    $("#tigerInfo").hide();
-    $("#bearInfo").hide();
-  });
-  $("#tiger").click(function() {
-    $("#lionInfo").hide();
-    $("#tigerInfo").show();
-    $("#bearInfo").hide();
-  });
-  $("#bear").click(function() {
-    $("#lionInfo").hide();
-    $("#tigerInfo").hide();
-    $("#bearInfo").show();
-  });
-
-});
+Look what I can do!
